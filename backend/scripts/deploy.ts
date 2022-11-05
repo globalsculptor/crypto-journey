5746b466aba1b8cbf6699972ce027ebef5f6dfb0
Jorge <www.jorge97@gmail.com>
2022-11-05 10:21:23 -0700

    clean up

--- backend/scripts/deploy.ts
+++ backend/scripts/deploy.ts
@@ -1,15 +1,12 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const initial_amount = 1_000_000;
 
   const ERC20 = await ethers.getContractFactory("ERC20");
   const TokenContract = await ERC20.deploy("CryptoJourney", "JRNY");
 
   await TokenContract.deployed();
-  console.log(
-    `Contract has been deployed successfully with ${initial_amount} initial tokens`
-  );
+  console.log(`Contract has been deployed successfully`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
