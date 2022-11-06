7329b10498567100b9c39ca5763f8e50ce9fb757
Jacob <jacob.ilin.us@gmail.com>
2022-11-05 19:51:34 -0700

    web3 connect

---
+++ Crypto Journey/Assets/Scripts/Web3Button.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// use web3.jslib
+using System.Runtime.InteropServices;
+using TMPro;
+
+public class Web3Button : MonoBehaviour
+{
+    // text in the button
+    public TextMeshProUGUI ButtonText;
+    // use WalletAddress function from web3.jslib
+    [DllImport("__Internal")] private static extern string WalletAddress();
+
+    public void OnClick()
+    {
+        // get wallet address and display it on the button
+        ButtonText.text = WalletAddress();
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
